49995c37b7b292a015a128f9728e71aa842bdf48
SW <aliwo@naver.com>
2025-01-17 14:35:47 +0900

    add unique constraint

--- temp.py
+++ temp.py
@@ -1,11 +1,4 @@
-def abc(a: bool, b: bool) -> int | None:
-    if a:
-        return 123
-    if b:
-        return 12
-    return None
-
-
-result = abc(True, False)
-if result:
-    print(456 + result)
+try:
+    my_int = int("a")
+except ValueError:
+    print("erorr catch")
